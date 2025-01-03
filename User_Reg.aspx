c292a17446f554e337a23a179e940aab5cceaf43
Nehanbc <126695206+Nehanbc@users.noreply.github.com>
2025-01-03 13:20:19 +0530

    Add files via upload

---
+++ User_Reg.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="User_Reg.aspx.cs" Inherits="WebApplication1neha.User_Reg" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            width: 191px;
+        }
+    </style>
+</head>
+<body style="width: 809px; height: 177px">
+    <form id="form1" runat="server">
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style2">StudentID</td>
+                <td>
+                    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                </td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style2">StudentName</td>
+                <td>
+                    <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                </td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style2">Age</td>
+                <td>
+                    <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+                </td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style2">Contact</td>
+                <td>
+                    <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+                </td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+        </table>
+        <p>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Btn_insert" runat="server" Height="29px" OnClick="Btn_insert_Click" Text="Insert" Width="71px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Btn_Update" runat="server" Height="29px" OnClick="Btn_update_Click" Text="Update" Width="71px" />
+&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Btn_delete" runat="server" Height="30px" OnClick="Btn_delete_Click" Text="Delete" Width="79px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Btn_search" runat="server" Height="32px" OnClick="Btn_search_Click" Text="Search" Width="83px" />
+        </p>
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ User_Reg.aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1neha
+{
+    public partial class User_Reg : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                LoadRecord();
+            }
+        }
+
+        protected void Btn_insert_Click(object sender, EventArgs e)
+        {
+            SqlConnection mysqlcon = new SqlConnection("Data Source=localhost\\SQLEXPRESS02;Initial Catalog=programming_db;Integrated Security=True");
+            mysqlcon.Open();
+
+            SqlCommand comm = new SqlCommand("Insert into StudentInfo values('"+int.Parse(TextBox1.Text)+"','"+TextBox2.Text+"','"+int.Parse(TextBox3.Text)+"','"+TextBox4.Text+"')", mysqlcon);
+            comm.ExecuteNonQuery();
+            mysqlcon.Close();
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Inserted');", true);
+            LoadRecord();
+        }
+
+
+        protected void Btn_update_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection mysqlcon = new SqlConnection("Data Source=localhost\\SQLEXPRESS02;Initial Catalog=programming_db;Integrated Security=True"))
+            {
+                mysqlcon.Open();
+                SqlCommand comm = new SqlCommand("UPDATE StudentInfo SET StudentName =  '"+TextBox2.Text+"',Age =  '"+TextBox3.Text+"',Contact =  '"+TextBox4.Text+"' where StudentID = "+int.Parse(TextBox1.Text)+"", mysqlcon);
+                comm.ExecuteNonQuery();
+                mysqlcon.Close();
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully updated');", true);
+                LoadRecord();
+            }
+            
+        }
+        private void LoadRecord()
+        {
+            using (SqlConnection mysqlcon = new SqlConnection("Data Source=localhost\\SQLEXPRESS02;Initial Catalog=programming_db;Integrated Security=True"))
+            {
+                mysqlcon.Open();
+                string query = "SELECT * FROM StudentInfo";
+                using (SqlCommand comm = new SqlCommand(query, mysqlcon))
+                {
+                    SqlDataAdapter sda = new SqlDataAdapter(comm);
+                    DataTable dt = new DataTable();
+                    sda.Fill(dt);
+
+                   
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+                }
+                //mysqlcon.Close();
+            }
+        }
+
+        protected void Btn_delete_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection mysqlcon = new SqlConnection("Data Source=localhost\\SQLEXPRESS02;Initial Catalog=programming_db;Integrated Security=True"))
+            {
+                mysqlcon.Open();
+                SqlCommand comm = new SqlCommand("Delete StudentInfo  where StudentID = "+int.Parse(TextBox1.Text)+"", mysqlcon);
+                comm.ExecuteNonQuery();
+                mysqlcon.Close();
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Deleted');", true);
+                LoadRecord();
+            }
+        }
+
+        protected void Btn_search_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection mysqlcon = new SqlConnection("Data Source=localhost\\SQLEXPRESS02;Initial Catalog=programming_db;Integrated Security=True"))
+            {
+                SqlCommand comm = new SqlCommand("select * from StudentInfo  where StudentID = "+int.Parse(TextBox1.Text)+"", mysqlcon);
+                SqlDataAdapter d = new SqlDataAdapter(comm);
+                DataTable dt = new DataTable();
+                d.Fill(dt);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+                
+            }
+    }
+}
